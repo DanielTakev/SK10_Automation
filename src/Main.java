eff546aeba3e381f037dc7a3dc0ce7ad6b46afa5
Daniel Takev <daniel.takev.vm@gmail.com>
2023-08-24 16:46:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a GitHub repository created for QA Automation course SK10
+        // SKILLO ACADEMY 2023
+    }
+}
