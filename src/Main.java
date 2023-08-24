9b98fd77484795928cb79e9b9ce05b38631e8bcf
Daniel Takev <daniel.takev.vm@gmail.com>
2023-08-24 16:59:25 +0300

    Added info comment

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // This is a GitHub repository created for QA Automation course SK10
         // SKILLO ACADEMY 2023
+        // Check all available branches to find sample solutions to your homework
     }
 }
