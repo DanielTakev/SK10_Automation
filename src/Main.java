f1bee0a8b3b6ca66962395e491450dd8009591af
lvsav <lvsavova93@gmail.com>
2023-08-25 12:13:52 +0300

    Added: * examples and exercises from lecture 2 ( Primitive data types and variables ) * examples from lecture 4 ( Operators, expressions, conditional statements and loops ) * removed .iml file and added to .gitignore

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // This is a GitHub repository created for QA Automation course SK10
-        // SKILLO ACADEMY 2023
-        // Check all available branches to find sample solutions to your homework
-    }
-}
