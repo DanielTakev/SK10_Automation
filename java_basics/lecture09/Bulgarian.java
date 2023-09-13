ef89bb8077c9eb03f4be99b9b11b033747d33da8
lvsav <lvsavova93@gmail.com>
2023-09-13 21:51:23 +0300

    Added practice from OOP principles in practice lecture

---
+++ java_basics/lecture09/Bulgarian.java
@@ -0,0 +1,21 @@
+package lecture09;
+
+public class Bulgarian extends Person {
+    public Bulgarian(String name, String sex, String job, String religion, String egn) {
+        super(name, sex, "Bulgarian", job, "Bulgarian", egn, "Bulgaria", religion);
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Здрасти!");
+    }
+
+    @Override
+    public Boolean isAdult() {
+        return this.age >= 18;
+    }
+
+    public void danceHoro() {
+        System.out.println("Аз танцувам хоро!");
+    }
+}
